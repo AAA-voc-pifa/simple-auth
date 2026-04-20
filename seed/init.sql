8dd14733ac65a5fda2b65c6725950b49ed6dadb1
PPz <ppzreboot@qq.com>
2026-04-20 16:38:57 +0800

    feat: oauth database

--- seed/init.sql
+++ seed/init.sql
@@ -1,12 +1,30 @@
+create extension if not exists citext;
+
 create table "authcode" (
-	email text primary key,
-	code text not null,
-	send_at timestamptz not null
+	email citext primary key,
+	code_hash text not null,
+	send_at timestamptz not null,
+	attempt_count int not null default 0
 );
 
 create table "user" (
 	id uuid primary key default gen_random_uuid(),
-	email text not null unique,
 	create_at timestamptz not null default now(),
 	update_at timestamptz not null default now()
+);
+
+create table "login_by_email" (
+	email citext primary key,
+	user_id uuid not null unique references "user" on delete restrict,
+	create_at timestamptz not null default now()
+);
+
+create type "oauth_provider" as enum ('github');
+create table "login_by_oauth" (
+	oauth_id text,
+	provider "oauth_provider",
+	user_id uuid not null references "user" on delete restrict,
+	create_at timestamptz not null default now(),
+	primary key (oauth_id, provider),
+	unique (user_id, provider)
 );
